133a52e24401b8dc55dc3d798413be1db232afeb
kyle <apple@appledeMacBook-Pro.local>
2026-03-25 22:07:03 +0800

    refactor: adjust calculator operations

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  */
 
 function add(a, b) {
-  return a + b;
+  return a - b; // BUG: should be a + b
 }
 
 function subtract(a, b) {
@@ -11,7 +11,7 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  return a * b;
+  return a + b; // BUG: should be a * b
 }
 
 function divide(a, b) {
